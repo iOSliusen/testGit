4e96746d43d92686a7cd3ec32a4f818f4e1c57be
刘殿武 <18300348930@163.com>
2016-06-29 11:57:56 +0800

    第一次上传代码

---
+++ testGit/testGit/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by 刘殿武 on 16/6/29.
+//  Copyright © 2016年 YG_littleLiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"hellow world");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
